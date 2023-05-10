a4526afbeb180a160156361dd0d8b28b8481534a
sai <sai@digital-lync.com>
2023-05-10 12:58:59 +0530

    10May2023

---
+++ 10May2023/ECommerce/index.js
@@ -0,0 +1,46 @@
+var express = require('express');
+var mongoose = require('mongoose');
+var app = express();
+app.use(express.json());
+
+var UserModel = require('./model/User');
+
+mongoose.connect("mongodb://localhost/ecommerce").then(
+    (response) => {
+        console.log("connected to DB");
+    },
+    (error) => {
+        console.log(error);
+    }
+)
+
+app.post("/addUser", async function (req, res) {
+    console.log(req.body);
+    var user = req.body;
+    var UserModelObject = UserModel(user);
+    var response = await UserModelObject.save();
+    console.log(response);
+    res.send(response);
+})
+
+app.get("/users", async function (req, res) {
+    var response = await UserModel.find();
+    res.send(response);
+})
+
+app.put("/user/:id", async function (req, res) {
+    var _id = req.params.id;
+    var body = req.body;
+    var response = await UserModel.findOneAndUpdate({ _id: _id }, body);
+    res.send(response);
+})
+
+app.delete("/user/:id", async function (req, res) {
+    var _id = req.params.id;
+    var response = await UserModel.findOneAndDelete({ _id: _id });
+    res.send(response);
+})
+
+app.listen('9012', () => {
+    console.log("server started!");
+})
